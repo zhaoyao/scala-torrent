1c714c6db012c3ff1af4f4a8a52d4efe6e7acd24
zhaoyao <zhaoyao.0xff@gmail.com>
2015-03-23 17:07:16 +0800

    Update unit test using WordSpecLike style

---
+++ src/test/scala/storrent/TorrentFilesSpec.scala
@@ -0,0 +1,50 @@
+package storrent
+
+import org.scalatest.{WordSpecLike, WordSpec, FunSuite, Matchers}
+
+import scala.io.Source
+
+/**
+ * User: zhaoyao
+ * Date: 3/12/15
+ * Time: 16:24
+ */
+class TorrentFilesSpec extends WordSpecLike with Matchers {
+
+  "From local files generate single file pieces" should {
+
+    "small piece length" in {
+
+      val targetFile = "src/test/resources/piecesTest/single_files/TestFile.txt"
+      val l = Source.fromFile(targetFile).mkString.length
+
+      val files = TorrentFiles.fromLocalFiles(targetFile, pieceLength = 1)
+
+      files.pieces.size shouldBe l
+    }
+
+    "large piece length" in {
+      val targetFile = "src/test/resources/piecesTest/single_files/TestFile.txt"
+      val files = TorrentFiles.fromLocalFiles(targetFile)
+      files.pieces.size shouldBe 1
+    }
+  }
+
+  "From local files generate multi file pieces" should {
+
+     "generate pieces" in {
+
+       val targetFile = "src/test/resources/piecesTest/multi_files"
+       val files = TorrentFiles.fromLocalFiles(targetFile, pieceLength = 3)
+
+       files.pieces.size shouldBe 14
+
+       // 跨越文件边界的piece
+       files.pieces(6).hash shouldEqual TorrentFiles.pieceHash("0\n1".getBytes, 3)
+       files.pieces(13).hash shouldEqual TorrentFiles.pieceHash("\n".getBytes, 1)
+
+     }
+  }
+
+
+}
